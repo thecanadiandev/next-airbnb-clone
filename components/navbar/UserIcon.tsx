6861dbb7e0956daa078ce1626d28f2c29a937079
Hari <harikrishkk@gmail.com>
2024-10-03 13:40:47 -0400

    commit 8: add dark mode

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import { LuUser2 } from 'react-icons/lu';
 
-const UserIcon = () => {
-  return <div>UserIcon</div>;
-};
-
+function UserIcon() {
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+}
 export default UserIcon;
