428f45ba60461fd201f700437873cf0cc9b6874e
Hari <harikrishkk@gmail.com>
2024-10-03 13:02:14 -0400

    commit 4: install shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+import { Button } from '@/components/ui/button';
+
 function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <>
+      <h1 className="text-3xl">HomePage</h1>
+      <div>
+        <Button variant="default" size="lg">
+          {' '}
+          Click me{' '}
+        </Button>
+      </div>
+    </>
+  );
 }
 export default HomePage;
