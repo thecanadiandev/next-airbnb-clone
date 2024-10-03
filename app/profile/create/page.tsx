61afa269ccfa90a6ca1ad73668e46dede463a9e1
Hari <harikrishkk@gmail.com>
2024-10-03 15:35:01 -0400

    commit 15: refactor create profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,28 +1,36 @@
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
 
 // server action.
-const createProfileAction = async (formData: FormData) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server';
   const firstName = formData.get('firstName') as string;
   console.log(firstName);
+  if (firstName !== 'shakeAndBake') {
+    return {
+      message: 'Unexpected error',
+    };
+  }
+  return {
+    message: 'Profile created..',
+  };
 };
 
 function CreateProfile() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor="firstName">First Name</Label>
-            <Input id="firstName" name="firstName" type="text" />
+      <div className="border p-8 rounded-md">
+        <FormContainer action={createProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="userName" label="User Name" />
           </div>
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
-        </form>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   );
