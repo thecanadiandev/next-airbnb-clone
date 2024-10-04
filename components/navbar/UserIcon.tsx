3912b21a2bcefdf76245fefd01d1ec28163fba35
Hari <harikrishkk@gmail.com>
2024-10-03 23:19:01 -0400

    commit 20: fetch profile image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,17 @@
+import { fetchProfileImage } from '@/utils/actions';
 import { LuUser2 } from 'react-icons/lu';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt="profile image"
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
