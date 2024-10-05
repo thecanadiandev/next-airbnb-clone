76cb7c1342aba3a1f9aef017183be0146ff58fa8
Hari <harikrishkk@gmail.com>
2024-10-05 19:39:46 -0400

    commit 38: Property Card

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -8,9 +8,9 @@ const PropertiesList = ({
   properties: PropertyCardProps[];
 }) => {
   return (
-    <section>
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
       {properties.map((property) => (
-        <PropertyCard key={property.id} />
+        <PropertyCard property={property} key={property.id} />
       ))}
     </section>
   );
