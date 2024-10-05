4efdc5236d17ac5998332b270001ec67a0537a2a
Hari <harikrishkk@gmail.com>
2024-10-05 19:55:02 -0400

    commit 39: Property rating & fav toggle

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
-};
-
+import { FaHeart } from 'react-icons/fa';
+import { Button } from '@/components/ui/button';
+function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  return (
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
+}
 export default FavoriteToggleButton;
--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -26,7 +26,7 @@ function PropertyCard({ property }: { property: PropertyCardProps }) {
           <h3 className="text-sm font-semibold mt-1">
             {name.substring(0, 30)}
           </h3>
-          {/* property rating */}
+          <PropertyRating inPage={false} propertyId={propertyId} />
         </div>
         <p className="text-sm mt-1 text-muted-foreground ">
           {tagline.substring(0, 40)}
@@ -40,7 +40,7 @@ function PropertyCard({ property }: { property: PropertyCardProps }) {
         </div>
       </Link>
       <div className="absolute top-5 right-5 z-5">
-        {/* favorite toggle button */}
+        <FavoriteToggleButton propertyId={propertyId} />
       </div>
     </article>
   );
