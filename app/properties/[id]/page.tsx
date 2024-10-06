1a3a5f64bf016b83f510145dc3c38b9dda65f204
Hari <harikrishkk@gmail.com>
2024-10-06 16:58:10 -0400

    commit 49: Share button

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import BreadCrumbs from '@/components/properties/Breadcrumbs';
+import ShareButton from '@/components/properties/ShareButton';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 
@@ -15,6 +16,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         <h1 className="text-4xl font-bold ">{property.tagline}</h1>
         <div className="flex items-center gap-x-4">
           {/* share button */}
+          <ShareButton name={property.name} propertyId={property.id} />
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
