c482879c4f7cacdfd1ae617da1c45d6c00a5ab21
Hari <harikrishkk@gmail.com>
2024-10-05 21:59:28 -0400

    commit 41: loading cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import LoadingCards from '@/components/card/LoadingCards';
 import CategoriesList from '@/components/home/CategoriesList';
 import PropertiesContainer from '@/components/home/PropertiesContainer';
+import { Suspense } from 'react';
 
 function HomePage({
   searchParams,
@@ -14,7 +16,13 @@ function HomePage({
         category={searchParams?.category}
         search={searchParams?.search}
       />
-      <PropertiesContainer />
+      <Suspense fallback={<LoadingCards />}>
+        {/* Only if we fetch inside a the child, Suspense works.*/}
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   );
 }
