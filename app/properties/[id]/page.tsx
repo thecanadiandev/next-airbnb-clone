f68f6240f2ecb2d6c2db27b6a1bb58b740f7d4bb
Hari <harikrishkk@gmail.com>
2024-10-06 16:47:03 -0400

    commit 48: breadcrumb component

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,24 @@
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import BreadCrumbs from '@/components/properties/Breadcrumbs';
+import { fetchPropertyDetails } from '@/utils/actions';
+import { redirect } from 'next/navigation';
+
+async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect('/');
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold ">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
+}
+export default PropertyDetailsPage;
