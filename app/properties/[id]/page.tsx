b6a2c5c9f085416b1c24f9ff08638f5157551fe3
Hari <harikrishkk@gmail.com>
2024-10-06 18:30:58 -0400

    commit 55: amenities

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import PropertyRating from '@/components/card/PropertyRating';
+import Amenities from '@/components/properties/Amenities';
 import BookingCalendar from '@/components/properties/booking/BookingCalendar';
 import BreadCrumbs from '@/components/properties/Breadcrumbs';
 import Description from '@/components/properties/Description';
@@ -42,6 +43,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <UserInfo profile={{ firstName, profileImage }} />
           <Separator className="mt-4" />
           <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           {/* calendar */}
