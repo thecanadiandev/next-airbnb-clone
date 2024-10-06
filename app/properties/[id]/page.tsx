84c43d0740c3edbb1ee71a6cec55bec4486966a4
Hari <harikrishkk@gmail.com>
2024-10-06 17:12:44 -0400

    commit 53: User info component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -5,6 +5,7 @@ import BreadCrumbs from '@/components/properties/Breadcrumbs';
 import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/ShareButton';
+import UserInfo from '@/components/properties/UserInfo';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 
@@ -13,6 +14,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -33,6 +36,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
             <PropertyRating inPage propertyId={property.id} />
           </div>
           <PropertyDetails details={details} />
+          <UserInfo profile={{ firstName, profileImage }} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           {/* calendar */}
