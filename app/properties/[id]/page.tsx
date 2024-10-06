6f3bc89175ef54507c18d78ba359eeb1d5877bbd
Hari <harikrishkk@gmail.com>
2024-10-06 17:02:35 -0400

    commit 50: image container

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,7 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import PropertyRating from '@/components/card/PropertyRating';
 import BreadCrumbs from '@/components/properties/Breadcrumbs';
+import ImageContainer from '@/components/properties/ImageContainer';
 import ShareButton from '@/components/properties/ShareButton';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
@@ -20,6 +22,19 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            {/* property rating */}
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/* calendar */}
+        </div>
+      </section>
     </section>
   );
 }
