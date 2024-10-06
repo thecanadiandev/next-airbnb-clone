31d2ef5f9cb84315c3003464ca527268d02bef1a
Hari <harikrishkk@gmail.com>
2024-10-06 15:16:27 -0400

    commit 46: toggle functionality

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -2,12 +2,12 @@ import { auth } from '@clerk/nextjs/server';
 import { CardSignInButton } from '../form/Buttons';
 import { fetchFavoriteId } from '@/utils/actions';
 import FavoriteToggleForm from './FavoriteToggleForm';
-function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
   const { userId } = auth();
   if (!userId) {
     return <CardSignInButton />;
   }
-  const favoriteId = fetchFavoriteId({ propertyId });
+  const favoriteId = await fetchFavoriteId({ propertyId });
 
   return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />;
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -210,7 +210,28 @@ export const toggleFavoriteAction = async (prevState: {
   favoriteId: string | null;
   pathname: string;
 }) => {
+  const user = await getAuthUser();
   const { propertyId, favoriteId, pathname } = prevState;
-  console.log(propertyId, favoriteId, pathname);
-  return { message: 'toggle favorite' };
+  console.log("FAV ID", favoriteId);
+  try {
+    if (favoriteId) {
+      await db.favorite.delete({
+        where: {
+          id: favoriteId,
+        },
+      });
+    } else {
+      await db.favorite.create({
+        data: {
+          propertyId,
+          profileId: user.id,
+        },
+      });
+    }
+    revalidatePath(pathname);
+    return { message: favoriteId ? 'Removed from favorites' : 'Added to favorites' };
+  } catch (error) {
+    return renderError(error);
+  }
+
 };
