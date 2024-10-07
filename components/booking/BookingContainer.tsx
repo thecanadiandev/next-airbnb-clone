adfd789d81fe41c233d263206229689aaca3a53c
Hari <harikrishkk@gmail.com>
2024-10-06 22:32:17 -0400

    commit 70: confirm booking

--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -4,8 +4,10 @@ import ConfirmBooking from './ConfirmBooking';
 import BookingForm from './BookingForm';
 
 const BookingContainer = () => {
-  const state = useProperty((state) => state);
-  console.log(state);
+  const { range } = useProperty((state) => state);
+  if (!range || !range.from || !range.to) return null;
+  if (range.to.getTime() === range.from.getTime()) return null;
+
   return (
     <div className="w-full">
       <BookingForm />
