55517a3bad9d110878a1735a4f1e16a1071a981d
Hari <harikrishkk@gmail.com>
2024-10-06 21:31:47 -0400

    commit 65: property rating average

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,14 +1,15 @@
+import { fetchPropertyRating } from '@/utils/actions';
 import { FaStar } from 'react-icons/fa';
 
 async function PropertyRating({
   inPage,
+  propertyId,
 }: {
   propertyId: string;
   inPage: boolean;
 }) {
-  // temp
-  const rating = 4.7;
-  const count = 100;
+  const { rating, count } = await fetchPropertyRating(propertyId);
+  if (count === 0) return null;
 
   const className = `flex gap-1 items-center ${inPage ? 'text-md' : 'text-xs'}`;
   const countText = count > 1 ? 'reviews' : 'review';
