7eb17b55738c4f56a8fafdd3147108e43f97380e
Hari <harikrishkk@gmail.com>
2024-10-06 21:43:09 -0400

    commit 66: allowing review

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,9 +11,10 @@ import UserInfo from '@/components/properties/UserInfo';
 import PropertyReviews from '@/components/reviews/PropertyReviews';
 import SubmitReview from '@/components/reviews/SubmitReview';
 import { Skeleton } from '@/components/ui/skeleton';
-import { fetchPropertyDetails } from '@/utils/actions';
+import { fetchPropertyDetails, findExistingReview } from '@/utils/actions';
 import { Separator } from '@radix-ui/react-dropdown-menu';
 import dynamic from 'next/dynamic';
+import { auth } from '@clerk/nextjs/server';
 
 import { redirect } from 'next/navigation';
 
@@ -33,6 +34,12 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
   const profileImage = property.profile.profileImage;
+
+  const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
+
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -64,7 +71,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <BookingCalendar />
         </div>
       </section>
-      <SubmitReview propertyId={property.id} />
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
       <PropertyReviews propertyId={property.id} />
     </section>
   );
