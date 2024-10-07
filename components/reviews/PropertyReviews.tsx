a4863966efa5d7578486c4a5207177c647474096
Hari <harikrishkk@gmail.com>
2024-10-06 20:24:45 -0400

    commit 60: submit review component

---
+++ components/reviews/PropertyReviews.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PropertyReviews = () => {
+  return <div>PropertyReviews</div>;
+};
+
+export default PropertyReviews;
--- utils/actions.ts
+++ utils/actions.ts
@@ -268,3 +268,21 @@ export const fetchPropertyDetails = (id: string) => {
     },
   });
 };
+
+// **************** REVIEWS  *********************
+
+export const createReviewAction = async () => {
+  return { message: 'create review' };
+};
+
+export const fetchPropertyReviews = async () => {
+  return { message: 'fetch reviews' };
+};
+
+export const fetchPropertyReviewsByUser = async () => {
+  return { message: 'fetch user reviews' };
+};
+
+export const deleteReviewAction = async () => {
+  return { message: 'delete  reviews' };
+};
